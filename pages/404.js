52e707d50b31eddcf5e30ed13eeb24bbbbf9e877
Ben Hoppe <moon@moontm.dev>
2022-08-06 16:56:18 +0200

    Fixed footer & added custom 404 Page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import {Navbar} from "../components/Navbar";
+import Link from "next/link";
+import {Footer} from "../components/Footer";
+import errorStyles from "../styles/error.module.css";
+
+export default function FourOhFour() {
+    return (
+        <div>
+            <Navbar/>
+            <h1 className={errorStyles.errorTitle}>Error 404 - Page Not Found :(</h1>
+            <p className={errorStyles.errorText}>Looks like the page you tried to access does not exist. If you think this is an error, please open an issue on <a href="https://github.com/Java-Discord/JavaDiscordWebsite">GitHub</a>.</p>
+            <Footer/>
+            </div>
+    )
+}
