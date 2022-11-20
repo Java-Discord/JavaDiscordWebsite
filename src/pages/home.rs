5c9bc126b701e66836b9c1bd181692ebeb1bdba7
-Denux <timon.klinkert@gmail.com>
2022-11-20 22:16:12 +0100

    work

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -6,12 +6,15 @@ pub(crate) fn home() -> Html {
     return html! {
     <>
         <Helmet>
-            <link rel="stylesheet" href="css/pages/home.css" />
+            <link rel="stylesheet" href="assets/css/pages/home.css" />
         </Helmet>
         //TODO implement navbar
         <div>
-            <div className={"introductionContainer"}>
-                <p>{"Test"}</p>
+            <div className="introductionContainer">
+                <div className="introductionText">
+                    <h1>{"HELP. "}<b style={{"color: #ff212d"}}>{"CODE. "}</b>{"LEARN."}</h1>
+                    <p className="introductionParagraph">{"With over X Members and X Messages sent each moth, join one of the biggest Java Communities on Discord to help, get help and discuss programming in Java."}</p>
+                </div>
             </div>
         </div>
     </>
